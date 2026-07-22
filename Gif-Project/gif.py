ee7390de044c080112da93577d7aae744afb8907
Lucas <lucasrian.dev@gmail.com>
2026-07-22 11:27:02 -0300

    projeto gif funcionando

--- Gif-Project/gif.py
+++ Gif-Project/gif.py
@@ -1,5 +1,12 @@
 import imageio.v3 as iio
 
 print("Funcionando!")
-imagem1 = iio.imread("team-pic1.png")
-print(imagem1.shape)
+imagem1 = ['team-pic1.png','team-pic2.png']
+img = []
+
+for imagem in imagem1:
+    img.append(iio.imread(imagem))
+
+print(len(img))
+iio.imwrite('team.gif', img, duration = 500, loop = 0)
+
