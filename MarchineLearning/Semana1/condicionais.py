962fbd04f27d0583f2d67d1183ead11a96cc176b
Lucas <lucasrian.dev@gmail.com>
2026-08-27 17:20:40 -0300

    Um pouco de base de sintaxe

---
+++ MarchineLearning/Semana1/condicionais.py
@@ -0,0 +1,31 @@
+"""
+bateria = 50
+
+if bateria >= 80:
+    print("Parar carregamento")
+elif bateria <= 75:
+    print("Iniciar carregamento")
+else: 
+    print("Manter estado atual")
+"""
+"""
+colunas = ["idade", "salario", "score"]
+for item in colunas:
+    print(item)
+
+for i in range(5):
+    print(i)
+
+# range(inicio, fim, passo)
+for i in range(0, 10, 2):
+    print(i)
+"""
+"""
+
+contador = 0
+while contador < 10:
+    print(contador)
+    contador += 1
+
+ 
+"""
---
+++ MarchineLearning/Semana1/variavel.py
@@ -0,0 +1,20 @@
+"""
+def soma(nu1, nu2):
+    calc = nu1 + nu2
+    return calc
+
+print(soma(5, 10))
+"""
+"""
+nomes = ["lucas", 'rian', 'damasceno']
+nomes.append("pires")
+print(nomes)
+"""
+"""
+usuarios = {
+    'nome': 'Lucas',
+    "profissao": "Desenvolvedor"
+}
+
+print(usuarios["profissao"])
+"""
