c0114039cad13acfc1fd9b0ce158db758c30c707
Lucas <lucasrian.dev@gmail.com>
2026-09-24 17:17:55 -0300

    Regressao linear

--- MarchineLearning/Semana4/teste.py
+++ MarchineLearning/Semana4/teste.py
@@ -1,9 +1,28 @@
 import pandas as pd
-import sklearn.datasets
+from sklearn.linear_model import LinearRegression
 
-data = sklearn.datasets.load_breast_cancer()
-print(data)
+# Creating the dataset
+data = pd.DataFrame({
+  'size_sqft': [450, 500, 600, 750, 800, 850, 900, 1000, 1100, 1200],
+  'floor':     [1, 2, 1, 3, 5, 4, 2, 6, 7, 10],
+  'bedrooms':  [1, 1, 1, 2, 2, 2, 2, 3, 3, 3],
+  'price':     [1200, 1300, 1450, 1600, 1700, 1750, 1800, 2000, 2150, 2300]
+})
 
-df = pd.DataFrame(data.data, columns=data.feature_names)
-df["target"] = data.target
-print(df)
+# Splitting the dataset into X and y
+X = data[['size_sqft', 'floor', 'bedrooms']]
+y = data['price']
+
+# Example: 950 sqft, 5th floor, 2 bedrooms
+new_apartment = pd.DataFrame({
+  'size_sqft': [950],
+  'floor': [5],
+  'bedrooms': [2]
+})
+
+
+my_modelo = LinearRegression()
+my_modelo.fit(X, y)
+
+prediction = my_modelo.predict(new_apartment)
+print(prediction)
